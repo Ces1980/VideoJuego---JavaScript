b35831e8ad8cdddd900f65503af48715b914f6a1
CesarVE <thecesarvargas@gmail.com>
2020-05-04 12:22:06 -0500

    Configurando el tablero del juego

--- js/js.js
+++ js/js.js
@@ -1,10 +1,34 @@
-// let canvas = document.getElementById('canvasSpace');
-// let ctx = canvas.getContext('2d');
-// ctx.fillText("HelloWorld", 10, 150);
 
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext('2d');
-canvas.height = 400;
+
+let canvas = document.createElement('canvas');
+let context = canvas.getContext('2d');
 canvas.width = 600;
+canvas.height = 400;
+// Creando un objeto de tipo Image
+let mainImage = new Image();
+// Declara falso la carga de la imagen
+mainImage.ready = false;
+// Cargar el objeto usando la función checkReady
+mainImage.onload = checkReady;
+// El recurso que va a usar el objeto cuando se cargue
+mainImage.src = '../img/pac.png';
+
+// Cuando se mande llamar la función va cambiar el valor de ready y al mismo tiempo va a mandar llamar la función de playgame
+function checkReady() {
+    this.ready = true;
+    playGame();
+}
+
+// playgame de momento solo va a mandar llmar al metodo render
+function playGame() {
+    render();
+}
+
+// El metodo render va a cambiar el lienzo de color y le va a dar dimenciones
+function render() {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+// Se incrusta canvas al documento
 document.body.appendChild(canvas);
 ctx.fillText('HelloWorld', 10, 150);
