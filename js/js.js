ecb51cf48cd3164a39a4f59bbc5676e2d47ae2f1
CesarVE <thecesarvargas@gmail.com>
2020-05-04 13:59:57 -0500

    Activivar el movimiento del personaje con el teclado(keyCode - keyClick)

--- js/js.js
+++ js/js.js
@@ -7,7 +7,8 @@ let player = {
     y: 100,
     pacmouth: 320,
     pacdir: 0,
-    psize: 32
+    psize: 32,
+    speed: 5
 }
 
 let canvas = document.createElement('canvas');
@@ -22,6 +23,31 @@ mainImage.ready = false;
 mainImage.onload = checkReady;
 // El recurso que va a usar el objeto cuando se cargue
 mainImage.src = '../img/pac.png';
+
+// Guardar los clicks
+var keyclick = {};
+
+// evento que esta a la escucha cuando se oprimen las teclas para generar movimiento
+document.addEventListener("keydown", function (event) {
+    keyclick[event.keyCode] = true;
+    move(keyclick);
+}, false);
+
+// Borrar los click previos para no hacer grande el arreglo, se borra al soltar el botòn
+document.addEventListener("keyup", function (event) {
+    delete keyclick[event.keyCode];
+}, false);
+
+
+// Al renderizar cargar los movimientos del jugador
+function move(keyclick) {
+    // (código del teclado al hacer click) { indicar movimiento en el plano}
+    if (37 in keyclick) { player.x -= player.speed; player.pacdir = 64; }
+    if (38 in keyclick) { player.y -= player.speed; player.pacdir = 96; }
+    if (39 in keyclick) { player.x += player.speed; player.pacdir = 0; }
+    if (40 in keyclick) { player.y += player.speed; player.pacdir = 32; }
+    render();
+}
 
 // Cuando se mande llamar la función va cambiar el valor de ready y al mismo tiempo va a mandar llamar la función de playgame
 function checkReady() {
@@ -50,4 +76,4 @@ function render() {
 }
 // Se incrusta canvas al documento
 document.body.appendChild(canvas);
-ctx.fillText('HelloWorld', 10, 150);
+
