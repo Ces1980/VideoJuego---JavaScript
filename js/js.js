6d2a91b7b342a05ff58b572812957900d4af5b21
CesarVE <thecesarvargas@gmail.com>
2020-05-04 12:01:52 -0500

    Usando JavaScript para crear canvas

--- js/js.js
+++ js/js.js
@@ -1,5 +1,10 @@
-let canvas = document.getElementById('canvasSpace');
-let ctx = canvas.getContext('2d');
-ctx.fillText("HelloWorld", 10, 150);
+// let canvas = document.getElementById('canvasSpace');
+// let ctx = canvas.getContext('2d');
+// ctx.fillText("HelloWorld", 10, 150);
 
-console.log("Desde la consola")
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+canvas.height = 400;
+canvas.width = 600;
+document.body.appendChild(canvas);
+ctx.fillText('HelloWorld', 10, 150);
